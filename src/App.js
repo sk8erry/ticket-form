de80c37518500ca9f4aa387fba5bbe437822b6fd
杨瑞奇 <grandruiqi@hotmail.com>
2019-04-09 20:29:26 -0700

    add subtotal

--- src/App.js
+++ src/App.js
@@ -14,16 +14,24 @@ class App extends Component {
   state = {
     cart: [],
     userPhone: '',
-    userEmail: ''
+    userEmail: '',
+    sponsors: []
   }
 
+  //Add one ticket (id) to cart
   handleAddOne = (item) => {
-		this.setState({
-      cart: [...this.state.cart, item.id]
-    })
-    console.log("ADD ONE")
-	}
+    let ticketCount = this.getTicketCount(item)
+    if (ticketCount > item.remaining - 1) { //item.remaining - 1 because we are about to put one in cart
+      return
+    } else {
+      this.setState({
+        cart: [...this.state.cart, item.id]
+      })
+    }
+  }
 
+
+  //Remove one ticket (id) from cart
 	handleRemoveOne = (item) => {
     let index = this.state.cart.indexOf(item.id);
     if (index === -1) {
@@ -38,20 +46,38 @@ class App extends Component {
     }
   }
 
-  handleEmailChange = (e) => {
-    console.log(e.target.value)
+  //Given ticket, return how many such tickets are in cart
+  getTicketCount = (item) => {
+    let ticketCount = 0
+    this.state.cart.forEach(ticketId => {
+      if (ticketId === item.id) {
+        ticketCount += 1
+      }
+    })
+    return ticketCount
   }
 
+  //Handle user input email
+  handleEmailChange = (e) => {
+    this.setState({
+      userEmail: e.target.value
+    })
+  }
+
+  //Handle user input phone number
   handlePhoneNumberChange = (e) => {
     this.setState({
       userPhone: e.target.value
     })
   }
 
+  handleSubmit = () => {
+    
+  }
+
   render() {
     const ticketsInfo = data.data
-    console.log("In cart:", this.state.cart)
-    console.log("Phone # is:", this.state.userPhone)
+    const cart = this.state.cart
     return (
       <div className="App">
         <div className="tickets-wrapper">
@@ -62,7 +88,7 @@ class App extends Component {
           <div className="customer-info-title">购票人信息</div>
           <CustomerInfo onEmailChange={this.handleEmailChange} onPhoneNumberChange={this.handlePhoneNumberChange}/>
         </div>
-        <Subtotal />
+        <Subtotal ticketsInfo={ticketsInfo} cart={cart} onSubmit={this.handleSubmit}/>
       </div>
     )
   }
--- src/components/Subtotal.js
+++ src/components/Subtotal.js
@@ -2,17 +2,31 @@ import React from 'react'
 import './Subtotal.css'
 
 class Subtotal extends React.Component {
+  getSum = (cart, ticketsInfo) => {
+    let sum = 0
+    cart.forEach(cartItem => {
+      ticketsInfo.forEach(ticketInfo => {
+        if (ticketInfo.id === cartItem) {
+          sum += (ticketInfo.price * 100)
+        }
+      })
+    })
+    return sum / 100
+  }
+
   render() {
+    const cart = this.props.cart
+    const ticketsInfo = this.props.ticketsInfo
     return (
       <React.Fragment>
         <div className="filler"></div>
         <div className="subtotal-wrapper">
           <div className="total">
-            <div className="total-tickets">共计</div>
-            <div className="sum">合计</div>
+            <div className="total-tickets">共计{cart.length}张</div>
+            <div className="sum">合计{this.getSum(cart, ticketsInfo)}元</div>
           </div>
           <div className="pay">
-            <button class="btn pay">
+            <button className="btn pay">
               立即支付
             </button>
           </div>
